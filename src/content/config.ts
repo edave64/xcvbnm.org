3f391b233ee6bfb4b975da98ff5b0e950274bae5
edave64 <github5815@xcvbnm.org>
2024-03-06 00:11:29 +0100

    Prettier fix

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,23 +3,25 @@ import { defineCollection, z } from 'astro:content';
 const blog = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
-	schema: ({image}) => z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: image().optional(),
-	}),
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			// Transform string to Date object
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: image().optional(),
+		}),
 });
 
 const projects = defineCollection({
 	type: 'content',
 	// Type-check frontmatter using a schema
-	schema: ({image}) => z.object({
-		name: z.string(),
-		originalRelease: z.coerce.date(),
-	}),
+	schema: ({ image }) =>
+		z.object({
+			name: z.string(),
+			originalRelease: z.coerce.date(),
+		}),
 });
 
 export const collections = { blog, projects };
